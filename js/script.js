40b6afa0145412c725bf6e4218b7c8d05be2e473
Vladislav <nicochan00@mail.ru>
2022-07-13 00:38:13 +0300

    Feature to change theme

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 'use strict';
 
 const btn = document.querySelector('#btn');
+const root = document.querySelector(':root');
+
+const themes = {
+   default: {
+      '--bg-color': '#eae9ea',
+      '--block-color': '#fff',
+      '--text-color': '#0b1f3b',
+      '--btn-color': '#263ce7',
+      '--tag-color': '#d1d7fa',
+   },
+   dark: {
+      '--bg-color': '#282b3c',
+      '--block-color': '#161e2b',
+      '--text-color': '#fff',
+      '--btn-color': '#263ce7',
+      '--tag-color': '#253044',
+   }
+};
 
 if (!localStorage.getItem('isDarkTheme')) {
    localStorage.setItem('isDarkTheme', false);
@@ -17,10 +35,15 @@ function btnHandler(event) {
 
    isDarkTheme = !isDarkTheme;
    localStorage.setItem('isDarkTheme', isDarkTheme);
-   
+
    changheTheme(isDarkTheme);
 }
 
 function changheTheme(isDarkTheme) {
-   
+   const theme = isDarkTheme ? 'dark' : 'default';
+   console.log(theme);
+
+   Object.entries(themes[theme]).forEach(([key, value]) => {
+      root.style.setProperty(key, value);
+   });
 }
