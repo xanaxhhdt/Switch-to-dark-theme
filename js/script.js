251865aaecc0d82288cd936c1842602888541740
Vladislav <nicochan00@mail.ru>
2022-07-13 00:22:20 +0300

    variable change isDarkAge

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const btn = document.querySelector('#btn');
+
+if (!localStorage.getItem('isDarkTheme')) {
+   localStorage.setItem('isDarkTheme', false);
+}
+
+let isDarkTheme = JSON.parse(localStorage.getItem('isDarkTheme'));
+
+changheTheme(isDarkTheme);
+
+btn.addEventListener('click', btnHandler);
+
+function btnHandler(event) {
+   event.preventDefault();
+
+   isDarkTheme = !isDarkTheme;
+   localStorage.setItem('isDarkTheme', isDarkTheme);
+   
+   changheTheme(isDarkTheme);
+}
+
+function changheTheme(isDarkTheme) {
+   
+}
